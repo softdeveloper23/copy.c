d0bb3e7a28e31c040581dfa7500dbf49bc982a49
unknown <brannongarrett77@gmail.com>
2023-12-12 14:08:23 +0100

    Added if statements and free memory

--- copy.c
+++ copy.c
@@ -7,13 +7,18 @@
 int main(void)
 {
     char *s = get_string("s: "); // get_string returns a pointer to a string
+    if (s == NULL)               // if get_string returns NULL, it means it failed to allocate memory
+    {
+        return 1;
+    }
 
     char *t = malloc(strlen(s) + 1); // +1 for the null terminator
-
-    for (int i = 0, n = strlen(s); i <= n; i++)
+    if (t == NULL)                   // if malloc returns NULL, it means it failed to allocate memory
     {
-        t[i] = s[i];
+        return 1;
     }
+
+    strcpy(t, s); // copy string s into string t
 
     if (strlen(t) > 0)
     {
@@ -22,4 +27,7 @@ int main(void)
 
     printf("s: %s\n", s);
     printf("t: %s\n", t);
+
+    free(t); // free memory allocated by malloc
+    return 0;
 }
